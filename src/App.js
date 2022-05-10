cc403659a6498a9c0f9c7ba7c0d089c88d3d94a8
khalid umar <dreamiwoarld.online@gmail.com>
2022-05-10 23:15:15 +0600

    initilize dasiy ui and setup navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home/Home";
+import Navbar from "./Pages/Shared/Navbar/Navbar";
 
 function App() {
   return (
     <div className='App'>
-      <h1 className='text-purple-600'>intialize project with tailwind</h1>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+      </Routes>
     </div>
   );
 }
