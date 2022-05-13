8c50b3e8372843dce79d77d326939fffe7b5c23b
khalid umar <dreamiwoarld.online@gmail.com>
2022-05-14 00:29:33 +0600

    setup react hooks form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,52 @@
 import React from "react";
-
+import { FcGoogle } from "react-icons/fc";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init.js";
+import { useForm } from "react-hook-form";
 const Login = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  if (user) {
+    console.log(user);
+  }
+  console.log(loading, error);
   return (
-    <div>
-      <h1>this is login page</h1>
+    <div className='flex h-screen justify-center items-center'>
+      <div className='card w-96 bg-base-100 shadow-xl'>
+        <div className='card-body'>
+          <h2 className='text-center text-2xl font-bold'>Login</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("firstName", { required: true })} />
+            {errors.firstName?.type === "required" && "First name is required"}
+
+            <input {...register("lastName", { required: true })} />
+            {errors.lastName && "Last name is required"}
+
+            <input
+              type='submit'
+              className='btn btn-outline btn-accent w-full'
+              value='LOGIN'
+            />
+          </form>
+          <div className='divider'>OR</div>
+          <button
+            onClick={() => signInWithGoogle()}
+            className='btn btn-outline btn-primary uppercase'
+          >
+            <FcGoogle className='mr-2 text-xl' />
+            continue with google
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
