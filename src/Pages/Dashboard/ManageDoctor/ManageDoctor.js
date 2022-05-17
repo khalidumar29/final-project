1b0d5bc1b04461d34609747f96c93f1c55708a86
khalid umar <dreamiwoarld.online@gmail.com>
2022-05-18 00:43:23 +0600

    done doctor manage

--- src/Pages/Dashboard/ManageDoctor/ManageDoctor.js
+++ src/Pages/Dashboard/ManageDoctor/ManageDoctor.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Loading from "../../Shared/Loading/Loading";
 
 const ManageDoctor = () => {
+  const { data: doctors, isLoading } = useQuery("doctors", () =>
+    fetch("http://localhost:3100/doctor", {
+      authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+    }).then((res) => res.json())
+  );
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
   return (
     <div>
-      <h1 className='text-2xl'>this manage doctor</h1>
+      <h1 className='text-2xl capitalize'>manage doctor: {doctors.length}</h1>
     </div>
   );
 };
